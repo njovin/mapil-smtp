70bfdd2fd090aa4274bea7b4298bebbf8b87485a
Nathan Jovin <nathanjovin@gmail.com>
2016-04-02 13:16:37 -0400

    basic mail parsing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var smtpServer = require('smtp-server').SMTPServer;
+var MailParser = require("mailparser").MailParser;
+var mailparser = new MailParser();
 
 var server = new smtpServer({
 	name: "mapil-smtp",
@@ -9,6 +11,17 @@ var server = new smtpServer({
 		console.log('from ' + address.address);
 		return cb();
 	},
+	onData: function(stream, session, callback){
+		var email = '';
+		stream.on('data', function(buffer) {
+		var part = buffer.toString();
+			email += part;
+		});
+		stream.on('end', function() {
+	        	mailparser.write(email);
+        		mailparser.end();	
+		});
+	},
 	onRcptTo: function(address, session, cb) {
 		console.log('recip to ' + address.address);
 	        if(address.address.substr(-14) !== '@mail.mapil.co'){
@@ -17,4 +30,14 @@ var server = new smtpServer({
 		return cb();
 	}
 });
+
+mailparser.on("end", function(mail_object){
+console.log(JSON.stringify(mail_object));
+//    console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
+ //   console.log("Subject:", mail_object.subject); // Hello world!
+  //  console.log("Text body:", mail_object.text); // How are you today?
+});
+
+
+
 server.listen(25);
