1444339c11121ccd658ad059e55d33c1ece074e6
Nathan Jovin <njovin@zingle.me>
2016-04-18 11:59:42 -0700

    update pm2 command. clean up code

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var env = require('node-env-file');
 var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
 var smtpServer = require('smtp-server').SMTPServer;
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
-//var mongoose = require('mongoose');
 
+// setup the server listener
 env(__dirname + '/.env');
 var server = new smtpServer({
 	name: "mapil-smtp",
@@ -36,24 +35,27 @@ var server = new smtpServer({
 	}
 });
 
+// handle the end of mail input
 mailparser.on("end", function(mail_object){
-for(var x in mail_object.attachments) {
-mail_object.attachments[x].content = null;
-}
-MongoClient.connect(process.env.MONGO_URL, function(err, db) {
-console.log(err);
-   db.collection('emails').insertOne(mail_object
-   , function(err, result) {
-	console.log(err);
-  });
-});
-console.log(JSON.stringify(mail_object));
-    console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
-   console.log("Subject:", mail_object.subject); // Hello world!
-  console.log("Text body:", mail_object.text); // How are you today?
-console.log(JSON.stringify(mail_object));
-});
+    
+    // wipe the content of the attachments - we don't store these at this time
+    for(var x in mail_object.attachments) {
+        mail_object.attachments[x].content = null;
+    }
 
+    // connect to mongo
+    MongoClient.connect(process.env.MONGO_URL, function(err, db) {
+        // log any errors
+        if(err) {
+            console.log(err);
+            return;
+        }
 
+        // insert the record
+        db.collection('emails').insertOne(mail_object, function(err, result) {
+            if(err) console.log(err);
+        });
+    });
+});
 
 server.listen(25);
