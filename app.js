ca9af99d42c2064063415ea01d373f13ce165710
Nathan Jovin <nathanjovin@gmail.com>
2016-04-18 00:44:15 -0400

    mongo updates

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+var env = require('node-env-file');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 var smtpServer = require('smtp-server').SMTPServer;
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
+//var mongoose = require('mongoose');
 
+env(__dirname + '/.env');
 var server = new smtpServer({
 	name: "mapil-smtp",
 	banner: "mail.mapil.co",
-	size: 1048576,
+	size: 10485760,
 	disabledCommands: ['AUTH'],
 	onMailFrom: function(address, session, cb) {
 		console.log('from ' + address.address);
@@ -32,10 +37,21 @@ var server = new smtpServer({
 });
 
 mailparser.on("end", function(mail_object){
+for(var x in mail_object.attachments) {
+mail_object.attachments[x].content = null;
+}
+MongoClient.connect(process.env.MONGO_URL, function(err, db) {
+console.log(err);
+   db.collection('emails').insertOne(mail_object
+   , function(err, result) {
+	console.log(err);
+  });
+});
 console.log(JSON.stringify(mail_object));
-//    console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
- //   console.log("Subject:", mail_object.subject); // Hello world!
-  //  console.log("Text body:", mail_object.text); // How are you today?
+    console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
+   console.log("Subject:", mail_object.subject); // Hello world!
+  console.log("Text body:", mail_object.text); // How are you today?
+console.log(JSON.stringify(mail_object));
 });
 
 
