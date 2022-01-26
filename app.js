4da247b608317c2201635f7c420a3df17434f9ea
Nathan Jovin <nathanjovin@gmail.com>
2022-01-25 17:36:53 -0800

    Fix request payload

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function storeEmail(mail_object)
         // insert the record
         db.collection(process.env.MONGO_MESSAGE_COLLECTION).insertOne(mail_object, function(err, result) {
             if(err) console.log(err);
-            request.post({uri: 'https://mapil.co/internal/webhook', body: mail_object, json:true}, mail_object, function (error, response, body) {
+            request.post({uri: 'https://mapil.co/internal/webhook', body: mail_object, json:true}, function (error, response, body) {
                 console.error('error:', error); // Print the error if one occurred
                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                 console.log('body:', body); // Print the HTML for the Google homepage.
