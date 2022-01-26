1d6f0adcb2f3a9778c623ca29b2a41b16854ca3e
Nathan Jovin <nathanjovin@gmail.com>
2022-01-26 04:57:47 -0800

    revert upgrades

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var smtpServer = require('smtp-server').SMTPServer;
 var MailParser = require("mailparser").MailParser;
 var pg = require('pg');
 var http = require('http');
-var https = require('https');
 
 // load the .env file, if any
 env(__dirname + '/.env');
@@ -78,17 +77,6 @@ function storeEmail(mail_object)
         // insert the record
         db.collection(process.env.MONGO_MESSAGE_COLLECTION).insertOne(mail_object, function(err, result) {
             if(err) console.log(err);
-            console.log('requesting');
-            console.log(JSON.stringify(mail_object));
-            console.log( mail_object._id);
-            console.log("https://mapil.co/internal/smtp-webhook?id=" + mail_object._id);
-            https.get("https://mapil.co/internal/smtp-webhook?id=" + mail_object._id, function(res) {
-                console.log('webhook ok');
-            }).on('error', function(e) {
-                console.log('webhook not ok');
-                console.log(e);``
-            });
-
         });
     });
 }
